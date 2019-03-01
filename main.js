41d909a24e228d6bb37a936c75714ea6782cb1eb
Markus-Rost <sukramxro@gmail.com>
2019-03-01 10:19:53 +0100

    test

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ app.use(bodyParser.json());
 
 app.post('/', function (req, res) {
 
-    console.log(req,'-----------------------',res,'-----------------------',req.body);
+    console.log(req,'-----------------------',res);
 
     res.json({
         message: 'ok got it!'
